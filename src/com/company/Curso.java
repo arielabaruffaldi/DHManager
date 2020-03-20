755d346e95e2b423bcad7657a81972c0cde5e844
Ariela <ariela@Arielas-MacBook-Pro.local>
2020-03-20 20:36:25 -0300

    Parte E: agrego variables en el Curso

--- src/com/company/Curso.java
+++ src/com/company/Curso.java
@@ -1,12 +1,27 @@
 package com.company;
 
+import com.company.Profesor.ProfesorAdjunto;
+import com.company.Profesor.ProfesorTitular;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Curso {
     private String nombre;
     private Integer codigoCurso;
+    private ProfesorTitular unProfesorTitular;
+    private ProfesorAdjunto unProfesorAdjunto;
+    private Integer cupoMaximoAlumnos;
+    private List<Alumno> unaListaDeAlumno;
 
-    public Curso(String nombre, Integer codigoCurso) {
+    public Curso(String nombre, Integer codigoCurso, ProfesorTitular unProfesorTitular,
+                 ProfesorAdjunto unProfesorAdjunto, Integer cupoMaximoAlumnos, List<Alumno> unaListaDeAlumno) {
         this.nombre = nombre;
         this.codigoCurso = codigoCurso;
+        this.unProfesorTitular = unProfesorTitular;
+        this.unProfesorAdjunto = unProfesorAdjunto;
+        this.cupoMaximoAlumnos = cupoMaximoAlumnos;
+        this.unaListaDeAlumno = new ArrayList<>();
     }
 
     public String getNombre() {
@@ -15,6 +30,22 @@ public class Curso {
 
     public Integer getCodigoCurso() {
         return codigoCurso;
+    }
+
+    public ProfesorTitular getUnProfesorTitular() {
+        return unProfesorTitular;
+    }
+
+    public ProfesorAdjunto getUnProfesorAdjunto() {
+        return unProfesorAdjunto;
+    }
+
+    public Integer getCupoMaximoAlumnos() {
+        return cupoMaximoAlumnos;
+    }
+
+    public List<Alumno> getUnaListaDeAlumno() {
+        return unaListaDeAlumno;
     }
 
     @Override
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,12 +10,5 @@ public class Main {
         Alumno unAlumno = new Alumno("Pepito", "Suarez", 1);
         Alumno otroAlumno = new Alumno("Juan", "Carlos", 2);
 
-        Curso unCurso = new Curso ("1B", 1);
-        Curso otroCurso = new Curso ("2B", 2);
-
-        Profesor unProfesor = new Profesor("Maria", "Apellidosky", 1, 2);
-        Profesor otroProfesor = new Profesor("Jose", "Rodo", 1, 4);
-
-        System.out.println(unProfesor.equals(otroProfesor));
     }
 }
