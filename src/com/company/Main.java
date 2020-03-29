6b8fdfce76353604548d93a0fbd7413253987589
arielapaula <abaruffaldi@glamit.com.ar>
2020-03-29 19:39:23 -0300

    Ultimos retoques en main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,26 +11,38 @@ public class Main {
         DigitalHouseManager unDigitalHouseManager = new DigitalHouseManager();
         unDigitalHouseManager.altaProfesorTitular("mariano", "otero", 0, "Matematica");
         unDigitalHouseManager.altaProfesorAdjunto("josefina", "echerri", 1, 2);
+        unDigitalHouseManager.altaProfesorTitular("evaristo", "pizza", 2, "Lengua");
+        unDigitalHouseManager.altaProfesorAdjunto("angeles", "pepe", 3, 10);
+
         unDigitalHouseManager.altaCurso("Full Stack", 20001, 3);
         unDigitalHouseManager.altaCurso("Android", 20002, 2);
+
+        unDigitalHouseManager.asignarProfesores(20002, 0, 1);
+        unDigitalHouseManager.asignarProfesores(20001, 2, 3);
+
+
         unDigitalHouseManager.altaAlumno("Pepe", "Suarez", 0);
         unDigitalHouseManager.altaAlumno("Maria", "Pepita", 1);
         unDigitalHouseManager.altaAlumno("Jonathan", "Algo", 2);
-        unDigitalHouseManager.asignarProfesores(20002, 0, 1);
-        unDigitalHouseManager.asignarProfesores(20001, 0, 1);
+
         unDigitalHouseManager.inscribirAlumno(0, 20001);
         unDigitalHouseManager.inscribirAlumno(1, 20001);
+
         unDigitalHouseManager.inscribirAlumno(0, 20002);
         unDigitalHouseManager.inscribirAlumno(1, 20002);
         unDigitalHouseManager.inscribirAlumno(2, 20002);
+
         unDigitalHouseManager.bajaProfesor(2);
+
+        unDigitalHouseManager.bajaCurso(20002);
+
         Alumno unAlumno = unDigitalHouseManager.buscarAlumno(0);
+
         System.out.println(unAlumno.getListaDeCursos());
 
         //PARTE K: para poder acceder a los cursos de un alumno le agregué el atributo listaDeCursos a la clase Alumno
         // y el método agregarCursoAlAlumno que se ejecuta en el método inscribirAlumno() de DigitalHouseManager,
         // entonces cada vez que se inscribe a un alumno, este agrega un nuevo elemento al conjunto listaDeCursos.
 
-        System.out.println(unDigitalHouseManager.buscarAlumno(2010));
     }
 }
